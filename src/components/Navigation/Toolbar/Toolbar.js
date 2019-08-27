5bc9dd61cdec5ebcc79488868ace91d7becd6796
ANANDKNIT <anand.sharma0510@gmail.com>
2019-08-28 01:13:12 +0530

    added navigation to burger builder

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,12 +1,15 @@
 import React from "react";
-import classes from "./Toolbar.css"
-const toolbar = (props) =>(
-    <header className={classes.Toolbar}>
-        <div>Menu</div>
-        <div>Logo</div>
-        <nav></nav>
-        </header>
-
-)
+import classes from "./Toolbar.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+const toolbar = props => (
+  <header className={classes.Toolbar}>
+    <div>Menu</div>
+    <Logo />
+    <nav>
+      <NavigationItems />
+    </nav>
+  </header>
+);
 
 export default toolbar;
