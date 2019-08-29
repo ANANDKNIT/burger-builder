af28e219742548dae737d34729a355855d02f5b1
ANANDKNIT <anand.sharma0510@gmail.com>
2019-08-30 00:19:38 +0530

    added feature to opening and closing the drawer by clicking on Menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,14 +4,35 @@ import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const Layout = props => {
+class Layout extends React.Component {
+  state = {
+    showSideDrawer:true
+  }
+  sideDrawerClosedHandler= () => {
+this.setState({showSideDrawer:false})
+  }
+  sideDrawerOpenHandler= () => {
+    this.setState({showSideDrawer:true})
+  }
+  handleSideDrawerClick = () =>{
+    console.log("Clicked")
+this.setState((prevState)=>({showSideDrawer:!prevState.showSideDrawer}))
+  }
+
+  render(){
   return (
     <Aux>
-      <SideDrawer />
-      <Toolbar />
-      <main className={classes.Content}>{props.children}</main>
+      <SideDrawer 
+      closed={this.sideDrawerClosedHandler}
+      open={this.state.showSideDrawer}
+      />
+      <Toolbar 
+      clicked={this.handleSideDrawerClick}
+      />
+      <main className={classes.Content}>{this.props.children}</main>
     </Aux>
   );
+}
 };
 
 export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,16 +2,18 @@ import React from "react";
 import classes from "./Toolbar.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
-const toolbar = props => (
-  <header className={classes.Toolbar}>
-    <div>Menu</div>
-    <div className={classes.Logo}>
-      <Logo />
-    </div>
-    <nav>
-      <NavigationItems />
-    </nav>
-  </header>
-);
+const toolbar = props => {
+  return (
+    <header className={classes.Toolbar}>
+      <div onClick={props.clicked}>Menu</div>
+      <div className={classes.Logo}>
+        <Logo />
+      </div>
+      <nav className={classes.DasktopOnly}>
+        <NavigationItems />
+      </nav>
+    </header>
+  );
+};
 
 export default toolbar;
